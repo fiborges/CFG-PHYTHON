6505d3643698b822e98d9a90845f6d31393cbafa
Filipa Borges <filipaborges@outlook.pt>
2023-08-04 12:29:42 +0100

    Add files via upload

---
+++ 1-class/exercises.py
@@ -0,0 +1,22 @@
+#exercise 1.4
+cats = 10
+cans = 1
+
+result = cats * 1
+
+print("i need {} cans for {} cats. ".format(result, cats))
+
+days = 7
+
+result_days = days * result
+
+print("i need {} cans for {} cats for {} days ".format(result_days, cats, days))
+
+#exercise 1.5
+
+user_name = "filipa"
+age= 35
+
+result2= "{} is {} years old".format(user_name,age)
+print(result2)
+
