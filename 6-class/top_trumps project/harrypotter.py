36da8f5eaeb6d9cf62610acbe33673a43a1e5911
Sofia Balali <102326668+sofiabalali@users.noreply.github.com>
2023-08-31 21:40:44 +0200

    Update harrypotter.py

--- 6-class/top_trumps project/harrypotter.py
+++ 6-class/top_trumps project/harrypotter.py
@@ -8,10 +8,14 @@ import random
 def random_house():
    house_number =random.randint(1,4)
   # add api url =....
+requests= requests.get(url)
+house = response.json()
 
 #houses
-#1 1=='Gryffindor'
-#2 2== 'Slytherin'
-#3 3== 'Hufflepuff'
-#4 4== 'Ravenclaw'
+house_1==houses['Gryffindor']
+house_2== houses['Slytherin']
+house_3== houses['Hufflepuff']
+house_4== houses['Ravenclaw']
+
+###return{house??????? not sure }
 
