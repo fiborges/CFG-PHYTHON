8a628ac9ccca4d3b93e28af41a017d2bae12425e
Sofia Balali <102326668+sofiabalali@users.noreply.github.com>
2023-08-31 21:48:15 +0200

    Update harrypotter.py

--- 6-class/top_trumps project/harrypotter.py
+++ 6-class/top_trumps project/harrypotter.py
@@ -5,11 +5,32 @@
 import requests
 import random
 
-def random_house():
-   house_number =random.randint(1,4)
+
+#choose_house=input('what is your house?1,2,3,4')
+#need code to show what each house is like your pokemon numbering
+
+house_1==houses['Gryffindor']
+house_2== houses['Slytherin']
+house_3== houses['Hufflepuff']
+house_4== houses['Ravenclaw']
+if house_1:
+   print('welcome to Gryffindor')
+elif house_2:
+   print('welcome to Slytherin')
+elif house_3:
+   print('welcome to Hufflepuff')
+elif house_4:
+   print('welcome to Ravenclaw')
+else:
+   return 'unknown house'
+
+
+# if the house is random 
+#def random_house():
+   #house_number =random.randint(1,4)
   # add api url =....
-requests= requests.get(url)
-house = response.json()
+#requests= requests.get(url)
+#house = response.json()
 
 #houses
 house_1==houses['Gryffindor']
