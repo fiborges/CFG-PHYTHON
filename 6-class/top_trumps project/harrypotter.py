40c490cb579c3e332847cc6752484e1adab65946
Sofia Balali <102326668+sofiabalali@users.noreply.github.com>
2023-08-31 21:34:44 +0200

    Update harrypotter.py

--- 6-class/top_trumps project/harrypotter.py
+++ 6-class/top_trumps project/harrypotter.py
@@ -1,1 +1,17 @@
+#choose house
+#random character
+#spell
+
+import requests
+import random
+
+def random_house():
+   house_number =random.randint(1,4)
+  # add api url =....
+
+#houses
+#1 1=='Gryffindor'
+#2 2== 'Slytherin'
+#3 3== 'Hufflepuff'
+#4 4== 'Ravenclaw'
 
